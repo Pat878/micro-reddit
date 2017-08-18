7ff654fb6a87c506e59975645e4bb0652f96fe82
Patrick Mellon <mellon.patrick.j@gmail.com>
2017-08-18 18:23:18 -0400

    Add further validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
     belongs_to :user
     has_many :comments
+    validates :user_id, presence: true
     validates :title, presence: true, length: { maximum: 200 }
     validates :body, presence: true
 end
